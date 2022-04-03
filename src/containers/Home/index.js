896bb0015df21fc1a7c7beffce8111b0e65b7e08
giofanny.mowoka@pashouses.id <giofanny.mowoka@pashouses.id>
2022-04-04 05:20:14 +0700

    add home admnin

---
+++ src/containers/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <div class="container-fluid text-sm-center p-5 bg-light">
+        <h1 class="display-2">Welcome to Admin Dashboard</h1>
+        <p class="lead">admin dashboard to manage the ecommerce app!</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
